e750dbd1d72cf50caa09c1877fcac5c68aa1f3ec
Patrick Kalita <pkalita@lbl.gov>
2024-01-03 12:48:55 -0800

    Fix eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,15 +3,24 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
+  extends: [
     'plugin:react/recommended',
-    'eslint:recommended'
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
+    sourceType: 'module',
   },
+  plugins: ['@typescript-eslint'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
+  settings: {
+    react: {
+      version: "detect"
+    }
   }
 }
