1feaa373cc178e2958fedd20b7d324ffbe6e67ee
Patrick Kalita <pkalita@lbl.gov>
2024-01-03 12:29:28 -0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'nmdc-field-notes',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
