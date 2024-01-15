727f64e7e15962a846d34b7955c6bf3b602baa0f
eecavanna <eecavanna@users.noreply.github.com>
2024-01-15 14:28:25 -0800

    Configure "viewports" add-on

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,19 +1,24 @@
-import type {Preview} from "@storybook/react";
+import type { Preview } from "@storybook/react";
+import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
 
 const preview: Preview = {
-  // Note: This `parameters` object was generated automatically during Storybook initialization.
   parameters: {
+    // Note: This `actions` object was generated automatically during Storybook initialization.
     actions: { argTypesRegex: "^on[A-Z].*" },
+    // Note: This `controls` object was generated automatically during Storybook initialization.
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
     },
+    // Configure Storybook's "viewports" add-on, included with Storybook.
+    // Reference: https://storybook.js.org/docs/essentials/viewport#use-a-detailed-set-of-devices
+    viewport: {
+      viewports: INITIAL_VIEWPORTS,
+      defaultViewport: "iphone13",
+    },
   },
-  // Specify decorators we want Storybook to automatically apply to each story.
-  // Reference: https://storybook.js.org/docs/writing-stories/decorators#global-decorators
-  decorators: [],
 };
 
 export default preview;
