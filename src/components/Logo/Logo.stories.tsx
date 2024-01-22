604abebd8ed1da78f4c8d4187963045f0de337d7
eecavanna <eecavanna@users.noreply.github.com>
2024-01-21 16:23:02 -0800

    Implement story for `Logo` component

---
+++ src/components/Logo/Logo.stories.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+import Logo from "./Logo";
+import ionicContent from "../../lib/stories/decorators/ionicContent";
+
+// Export the component metadata as the default export.
+export default {
+  title: "Logo",
+  component: Logo,
+  decorators: [ionicContent]
+} as Meta<typeof Logo>;
+
+// Export each story.
+export const Default: StoryObj<typeof Logo> = {
+  render: (props) => <Logo {...props} />,
+};
