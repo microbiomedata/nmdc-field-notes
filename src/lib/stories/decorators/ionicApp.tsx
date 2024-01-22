488fdb183ab424d9488b9d644ef3d21565272345
eecavanna <eecavanna@users.noreply.github.com>
2024-01-21 16:23:30 -0800

    Implement `WelcomePage` story and an `ionicApp` decorator

---
+++ src/lib/stories/decorators/ionicApp.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { StoryFn } from "@storybook/react";
+import { IonApp, setupIonicReact } from "@ionic/react";
+
+/* Core CSS required for Ionic components to work properly */
+import "@ionic/react/css/core.css";
+
+/* Basic CSS for apps built with Ionic */
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
+
+/* Optional CSS utils that can be commented out */
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
+
+/* Theme variables */
+import "../../../theme/variables.css";
+
+setupIonicReact();
+
+/**
+ * Wraps the specified Story in some Ionic React elements, and returns the resulting React element.
+ *
+ * @param Story Storybook story you want to wrap
+ */
+const ionicApp = (Story: StoryFn) => {
+  return (
+    <IonApp>
+      <Story />
+    </IonApp>
+  );
+};
+
+export default ionicApp;
