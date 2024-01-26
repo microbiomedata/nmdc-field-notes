15bc3745de8c9556892aca5163fdf16a173b4283
eecavanna <eecavanna@users.noreply.github.com>
2024-01-25 19:17:34 -0800

    Delete example Storybook stories (simplify code review)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,7 +4,6 @@ const config: StorybookConfig = {
   stories: [
     "../src/**/*.mdx",
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-    "./example-stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
   addons: [
     "@storybook/addon-links",
