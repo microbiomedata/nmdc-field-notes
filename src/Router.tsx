f74a26da9a16fd7c10b5ba1f4a5ba84d1aedc8ce
eecavanna <eecavanna@users.noreply.github.com>
2024-02-24 19:19:49 -0800

    Implement tab-based navigation with placeholder content

--- src/Router.tsx
+++ src/Router.tsx
@@ -12,17 +12,22 @@ import { useStore } from "./Store";
 import LogoutPage from "./pages/LogoutPage/LogoutPage";
 import StudyViewPage from "./pages/StudyViewPage/StudyViewPage";
 import StudyCreatePage from "./pages/StudyCreatePage/StudyCreatePage";
+import TabNavigator from "./components/TabNavigator/TabNavigator";
+import PlaceholderPage from "./pages/PlaceholderPage";
 
 export const PATHS = {
   ROOT: "/",
-  HOME_PAGE: "/home",
+  HOME_PAGE: "/in/home",
   TUTORIAL_PAGE: "/tutorial",
   WELCOME_PAGE: "/welcome",
   LOGIN_PAGE: "/login",
   TOKEN_PAGE: "/token",
   LOGOUT_PAGE: "/logout",
-  STUDY_CREATE_PAGE: "/study/create",
-  STUDY_VIEW_PAGE: "/study/:id",
+  STUDY_CREATE_PAGE: "/in/study/create",
+  STUDY_VIEW_PAGE: "/in/study/:id",
+  TABBED_AREA: "/in",
+  GUIDE_PAGE: "/in/guide",
+  SETTINGS_PAGE: "/in/settings",
 };
 
 const Router: React.FC = () => {
@@ -45,16 +50,36 @@ const Router: React.FC = () => {
         <Route exact path={PATHS.TUTORIAL_PAGE}>
           <TutorialPage />
         </Route>
-        <AuthRoute path={PATHS.STUDY_VIEW_PAGE}>
-          <StudyViewPage />
-        </AuthRoute>
-        {/* Not sure why this needs to come after the view route, but it only works that way */}
-        <AuthRoute exact path={PATHS.STUDY_CREATE_PAGE}>
-          <StudyCreatePage />
-        </AuthRoute>
-        <AuthRoute exact path={PATHS.HOME_PAGE}>
-          <HomePage />
-        </AuthRoute>
+
+        <Route path={PATHS.TABBED_AREA}>
+          <TabNavigator>
+            <AuthRoute path={PATHS.STUDY_VIEW_PAGE}>
+              <StudyViewPage />
+            </AuthRoute>
+            {/* Not sure why this needs to come after the view route, but it only works that way */}
+            <AuthRoute exact path={PATHS.STUDY_CREATE_PAGE}>
+              <StudyCreatePage />
+            </AuthRoute>
+            <AuthRoute exact path={PATHS.HOME_PAGE}>
+              <HomePage />
+            </AuthRoute>
+            <AuthRoute exact path={PATHS.GUIDE_PAGE}>
+              <PlaceholderPage
+                title={"Guide"}
+                body={"Base route on Guide tab"}
+              />
+            </AuthRoute>
+            <AuthRoute exact path={PATHS.SETTINGS_PAGE}>
+              <PlaceholderPage
+                title={"Settings"}
+                body={"Base route on Settings tab"}
+              />
+            </AuthRoute>
+            {/* Fallback route for when the requested path doesn't match any of the above paths. */}
+            <Redirect to={PATHS.HOME_PAGE} />
+          </TabNavigator>
+        </Route>
+
         <Route exact path={PATHS.ROOT}>
           <Redirect to={apiToken ? PATHS.HOME_PAGE : PATHS.WELCOME_PAGE} />
         </Route>
