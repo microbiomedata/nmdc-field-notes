fb7756e75d5b12226f0aa6e5259b52ed7d7ea061
eecavanna <eecavanna@users.noreply.github.com>
2024-03-19 19:24:17 -0700

    Demonstrate "Markdown-in-JS" approach (using `react-markdown`)

---
+++ src/components/Tutorial/md-in-js/005.md.ts
@@ -0,0 +1,64 @@
+export const title = `Other fields in the NMDC Submission Portal that may be relevant to the sample or to later processing steps`;
+
+// language=Markdown
+const rawMarkdownContent = `
+- Source material identifier
+- Slope aspect
+- History/agrochemical additions
+- Aluminum saturation/extreme unusual properties
+- Aluminum saturation method/extreme unusual properties
+- Mean annual precipitation
+- Mean annual temperature
+- History/crop rotation
+- Current land use
+- Current vegetation
+- Current vegetation method
+- Drainage classification
+- Experimental factor
+- History/extreme events
+- Soil taxonomic/FAO classification
+- History/fire
+- History/flooding
+- Heavy metals/extreme unusual properties
+- Heavy metals method/extreme unusual properties
+- Soil horizon method
+- Link to classification method
+- Link to climate information
+- Soil taxonomic/local classification
+- Miscellaneous parameter
+- Soil taxonomic/local classification method
+- Oxygenation status of sample
+- History/previous land use method
+- History/previous land use
+- Profile position
+- Average seasonal precipitation
+- Mean seasonal temperature 
+- Slope gradient
+- Soil horizon
+- Soil texture measurement
+- Soil texture method
+- Soil type
+- Soil type method
+- Temperature
+- History/tillage
+- Water content
+- Water content method
+- pH method
+- Microbial biomass
+- Microbial biomass method
+- Carbon/nitrogen ratio
+- Organic matter
+- Organic nitrogen
+- Total carbon
+- Total nitrogen content
+- Total nitrogen content method
+- Total organic carbon
+- Total organic carbon method
+- Total phosphorus
+- Phosphate
+- Salinity
+`;
+
+export const markdownContent = rawMarkdownContent.trim();
+
+export default markdownContent;
