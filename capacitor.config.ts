7509ae45d9a85c4287c823c8d3b4284e9a2a26b2
eecavanna <eecavanna@users.noreply.github.com>
2024-03-25 16:12:25 -0700

    Customize app identifier

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "io.ionic.starter",
+  appId: "org.microbiomedata.fieldnotes",
   appName: "NMDC Field Notes",
   webDir: "dist",
   server: {
