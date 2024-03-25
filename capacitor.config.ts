3a108031126435db905ff48e59081ce2eeb73240
eecavanna <eecavanna@users.noreply.github.com>
2024-03-25 13:13:45 -0700

    Standardize app name to "NMDC Field Notes"

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
   appId: "io.ionic.starter",
-  appName: "Field Notes",
+  appName: "NMDC Field Notes",
   webDir: "dist",
   server: {
     androidScheme: "https",
