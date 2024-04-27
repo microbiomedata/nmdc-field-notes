2f3ef6de8d4ace04690551e86e84de1fa7042fb4
eecavanna <eecavanna@users.noreply.github.com>
2024-04-26 18:30:10 -0700

    Refactor code to use helper functions

--- src/components/ColorSchemeSelector/ColorSchemeSelector.tsx
+++ src/components/ColorSchemeSelector/ColorSchemeSelector.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ColorScheme } from "../../theme";
+import { activateColorScheme, ColorScheme } from "../../theme";
 import { IonSelect, IonSelectOption } from "@ionic/react";
 
 const ColorSchemeSelector: React.FC = () => {
@@ -7,22 +7,7 @@ const ColorSchemeSelector: React.FC = () => {
     <IonSelect
       label={"Color scheme"}
       placeholder={"Select color scheme"}
-      onIonChange={(event) => {
-        if (event.target.value === ColorScheme.Dark) {
-          document.documentElement.classList.add("ion-palette-dark");
-        } else if (event.target.value === ColorScheme.Light) {
-          document.documentElement.classList.remove("ion-palette-dark");
-        } else {
-          // Check whether the web browser indicates that the user prefers dark color schemes.
-          // TODO: Also, listen for changes to the system theme, in case the user changes it over time.
-          // Reference: https://developer.mozilla.org/en-US/docs/Web/API/Window/matchMedia#usage_notes
-          if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-            document.documentElement.classList.add("ion-palette-dark");
-          } else {
-            document.documentElement.classList.remove("ion-palette-dark");
-          }
-        }
-      }}
+      onIonChange={(event) => activateColorScheme(event.target.value)}
       value={undefined} // FIXME
     >
       <IonSelectOption value={ColorScheme.Dark}>
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -3,3 +3,66 @@ export enum ColorScheme {
   Light = "Light",
   System = "System",
 }
+
+/**
+ * Toggles the dark color scheme according to the flag passed in.
+ *
+ * Note: Activate/deactivating the dark color scheme is a matter of adding/removing
+ *       the `.ion-palette-dark` class from the `<html>` element.
+ *
+ * Reference: https://ionicframework.com/docs/theming/dark-mode#css-class
+ *
+ * @param wantsDarkColorScheme {boolean} Whether you want the dark color scheme to be enabled.
+ */
+export const toggleDarkColorScheme = (wantsDarkColorScheme: boolean) => {
+  const htmlEl = document.documentElement;
+  htmlEl.classList.toggle("ion-palette-dark", wantsDarkColorScheme);
+};
+
+/**
+ * Checks whether the system prefers the dark color scheme.
+ */
+export const checkWhetherSystemPrefersDarkness = () => {
+  return window.matchMedia("(prefers-color-scheme: dark)").matches;
+};
+
+/**
+ * Set up an event listener that keeps the app's color scheme in sync with the system preference.
+ */
+export const watchSystemColorSchemePreference = () => {
+  window
+    .matchMedia("(prefers-color-scheme: dark)")
+    .addEventListener("change", (event) => {
+      const wantsDarkness = event.matches;
+
+      // FIXME: Only do this if the user has selected the `System` color scheme.
+      if (true) {
+        toggleDarkColorScheme(wantsDarkness);
+      }
+    });
+};
+
+export const activateColorScheme = (colorScheme: ColorScheme) => {
+  switch (colorScheme) {
+    case ColorScheme.Light: {
+      toggleDarkColorScheme(false);
+      break;
+    }
+    case ColorScheme.Dark: {
+      toggleDarkColorScheme(true);
+      break;
+    }
+    case ColorScheme.System: {
+      const systemPrefersDarkColorScheme = checkWhetherSystemPrefersDarkness();
+      if (systemPrefersDarkColorScheme) {
+        toggleDarkColorScheme(true);
+      } else {
+        toggleDarkColorScheme(false);
+      }
+      break;
+    }
+    default: {
+      throw new Error(`Invalid color scheme: ${colorScheme}`);
+    }
+  }
+};
