decfa1e8948e9b8a5e533ffcf24b88a6d0db9e5c
Patrick Kalita <pkalita@lbl.gov>
2024-06-07 12:38:50 -0700

    Add User section to SettingsPage with login and logout buttons

--- src/pages/SettingsPage/SettingsPage.tsx
+++ src/pages/SettingsPage/SettingsPage.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import {
-  IonBackButton,
-  IonButtons,
   IonContent,
   IonHeader,
   IonItem,
@@ -9,31 +7,72 @@ import {
   IonList,
   IonNote,
   IonPage,
+  IonText,
   IonTitle,
+  useIonRouter,
 } from "@ionic/react";
 import ThemedToolbar from "../../components/ThemedToolbar/ThemedToolbar";
 import config from "../../config";
 import ColorPaletteModeSelector from "../../components/ColorPaletteModeSelector/ColorPaletteModeSelector";
+import SectionHeader from "../../components/SectionHeader/SectionHeader";
+import { useStore } from "../../Store";
+import paths from "../../paths";
+import { initiateLogin } from "../../auth";
 
 const SettingsPage: React.FC = () => {
+  const { loggedInUser, logout } = useStore();
+  const router = useIonRouter();
+
+  const handleLogout = async () => {
+    await logout();
+    router.push(paths.welcome, "none");
+  };
+
+  const handleLogin = () => {
+    return initiateLogin();
+  };
+
   return (
     <IonPage>
       <IonHeader>
         <ThemedToolbar>
-          <IonButtons slot={"start"}>
-            <IonBackButton defaultHref={"/"}></IonBackButton>
-          </IonButtons>
           <IonTitle>Settings</IonTitle>
         </ThemedToolbar>
       </IonHeader>
       <IonContent>
-        <IonList>
+        <SectionHeader>User</SectionHeader>
+        <IonList className="ion-padding-bottom">
+          {loggedInUser ? (
+            <>
+              <IonItem>
+                <IonLabel>
+                  <h3>Name</h3>
+                  <p>{loggedInUser.name}</p>
+                </IonLabel>
+              </IonItem>
+              <IonItem type="button" onClick={handleLogout}>
+                <IonText color="warning">Sign Out</IonText>
+              </IonItem>
+            </>
+          ) : (
+            <IonItem type="button" onClick={handleLogin}>
+              <IonText>Sign In</IonText>
+            </IonItem>
+          )}
+        </IonList>
+
+        <SectionHeader>About</SectionHeader>
+        <IonList className="ion-padding-bottom">
           <IonItem>
             <IonLabel>App version</IonLabel>
             <IonNote color={"medium"} slot={"end"}>
               {config.APP_VERSION}
             </IonNote>
           </IonItem>
+        </IonList>
+
+        <SectionHeader>Appearance</SectionHeader>
+        <IonList className="ion-padding-bottom">
           <IonItem>
             <ColorPaletteModeSelector />
           </IonItem>
