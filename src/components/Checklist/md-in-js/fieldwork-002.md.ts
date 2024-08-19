7fee8958bea9dd89791bd739cfefdcc5ee2bcfc8
yxu-lanl <38266469+yxu-lanl@users.noreply.github.com>
2024-08-19 12:30:21 -0600

    Update src/components/Checklist/md-in-js/fieldwork-002.md.ts
    Co-authored-by: eecavanna <134325062+eecavanna@users.noreply.github.com>

--- src/components/Checklist/md-in-js/fieldwork-002.md.ts
+++ src/components/Checklist/md-in-js/fieldwork-002.md.ts
@@ -11,7 +11,7 @@ const rawMarkdownContent = `
   * NESTEDLISTITEM Know how to read measurements from each specific tool you plan to use (e.g., compass, pH testing strips, maps)  
 * Consider setting up a study in the app prior to going out into the field  
 * Consider entering a test sample into the app to get familiar with the process and information needed  
-* Review the CARE principles for proper data sovereignty for indigenous lands and this figure from the Geological Society of America describes other aspects of [Field Ethics](https://www.geosociety.org/GSA/Education_Careers/GSA/edu-career/fieldethics.aspx?hkey=de4690fb-49ef-460a-8e86-21797ea969d8)
+* Review the CARE principles for proper data sovereignty for indigenous land. This figure from the Geological Society of America describes other aspects of [Field Ethics](https://www.geosociety.org/GSA/Education_Careers/GSA/edu-career/fieldethics.aspx?hkey=de4690fb-49ef-460a-8e86-21797ea969d8)
 
 ![Field Ethics and Sampling Checklist](assets/images/field-ethics.png)
 `;
