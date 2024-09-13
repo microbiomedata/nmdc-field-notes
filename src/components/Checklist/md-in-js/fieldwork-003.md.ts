da011f0f7ef9f47323487ac736f618f500ca6cd0
yxu-lanl <38266469+yxu-lanl@users.noreply.github.com>
2024-09-12 19:41:11 -0600

    Update src/components/Checklist/md-in-js/fieldwork-003.md.ts
    Co-authored-by: eecavanna <134325062+eecavanna@users.noreply.github.com>

--- src/components/Checklist/md-in-js/fieldwork-003.md.ts
+++ src/components/Checklist/md-in-js/fieldwork-003.md.ts
@@ -6,7 +6,7 @@ Schema for biosamples based on MIxS and other standards
 `;
 
 // Generate schemas Markdown from the TEMPLATES
-import {createSchemasMD} from './util';
+import { createSchemasMD } from './util';
 const rawMarkdownContent = createSchemasMD();
 
 export const markdownContent = rawMarkdownContent.trim();
