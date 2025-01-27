31d691b18a72f5d5a96cc6c0345912ba319b646b
eecavanna <eecavanna@users.noreply.github.com>
2025-01-27 12:05:19 -0800

    Display photo on Welcome screen and constrain card and button widths

--- src/pages/WelcomePage/WelcomePage.tsx
+++ src/pages/WelcomePage/WelcomePage.tsx
@@ -49,47 +49,76 @@ const WelcomePage: React.FC = () => {
               size={"12"}
               className={"ion-text-center ion-align-self-center"}
             >
-              <IonCard>
-                <IonCardHeader>
-                  <IonCardTitle>Welcome</IonCardTitle>
-                </IonCardHeader>
-                <IonCardContent className={classes.paddingBottomZero}>
-                  Welcome to NMDC Field Notes, an app designed to help you
-                  collect environmental metadata on&nbsp;the&nbsp;go.
-                </IonCardContent>
-                <IonButton
-                  fill={"clear"}
-                  href={"https://microbiomedata.org/field-notes/"}
+              <IonRow className={"ion-justify-content-center"}>
+                {/* Note: We included the seemingly-redundant size specifiers here (e.g. saying `size-lg="6"` also, even
+                          though we already said `size-md="6"`) because, without those intermediate specifiers, the
+                          resulting size seemed to be inherited from the `-xs` specification, instead of being inherited
+                          from the nearest specification smaller than the current viewport width. */}
+                <IonCol
+                  size-xs={"12"}
+                  size-sm={"12"}
+                  size-md={"6"}
+                  size-lg={"6"}
+                  size-xl={"4"}
                 >
-                  Learn more
-                </IonButton>
-              </IonCard>
+                  <IonCard>
+                    <img
+                      alt="Photo of researcher using the NMDC Field Notes iOS app on an Apple iPad"
+                      src="/person-using-ipad-1200x1200.jpg"
+                    />
+                    <IonCardHeader>
+                      <IonCardTitle>Welcome</IonCardTitle>
+                    </IonCardHeader>
+                    <IonCardContent className={classes.paddingBottomZero}>
+                      Welcome to NMDC&nbsp;Field&nbsp;Notes, an app designed to
+                      help you collect environmental metadata
+                      on&nbsp;the&nbsp;go.
+                    </IonCardContent>
+                    <IonButton
+                      fill={"clear"}
+                      href={"https://microbiomedata.org/field-notes/"}
+                    >
+                      Learn more
+                    </IonButton>
+                  </IonCard>
+                </IonCol>
+              </IonRow>
             </IonCol>
             <IonCol size={"12"} className={"ion-align-self-end"}>
-              <IonRow>
-                <IonCol size={"12"} className={"ion-text-center"}>
-                  <IonButton
-                    expand={"block"}
-                    className={"ion-margin-horizontal"}
-                    onClick={handleLogin}
-                  >
-                    <IonIcon
-                      icon={logIn}
-                      aria-hidden={"true"}
-                      slot={"start"}
-                    ></IonIcon>
-                    Log in with ORCiD
-                  </IonButton>
-                </IonCol>
-                <IonCol size={"12"} className={"ion-text-center"}>
-                  <IonButton
-                    fill={"clear"}
-                    id={"show-alert-for-continue-without-login"}
-                    expand={"block"}
-                    className={"ion-margin-horizontal"}
-                  >
-                    Continue without logging in
-                  </IonButton>
+              <IonRow className={"ion-justify-content-center"}>
+                <IonCol
+                  size-xs={"12"}
+                  size-sm={"12"}
+                  size-md={"6"}
+                  size-lg={"6"}
+                  size-xl={"4"}
+                >
+                  <IonRow>
+                    <IonCol size={"12"} className={"ion-text-center"}>
+                      <IonButton
+                        expand={"block"}
+                        className={"ion-margin-horizontal"}
+                        onClick={handleLogin}
+                      >
+                        <IonIcon
+                          icon={logIn}
+                          aria-hidden={"true"}
+                          slot={"start"}
+                        ></IonIcon>
+                        Log in with ORCiD
+                      </IonButton>
+                    </IonCol>
+                    <IonCol size={"12"} className={"ion-text-center"}>
+                      <IonButton
+                        fill={"clear"}
+                        id={"show-alert-for-continue-without-login"}
+                        expand={"block"}
+                        className={"ion-margin-horizontal"}
+                      >
+                        Continue without logging in
+                      </IonButton>
+                    </IonCol>
+                  </IonRow>
                 </IonCol>
               </IonRow>
             </IonCol>
