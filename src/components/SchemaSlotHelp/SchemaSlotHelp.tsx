baa68b8394f34efcfe0a8eda2b9ce8a44cf93e79
eecavanna <eecavanna@users.noreply.github.com>
2025-02-02 13:29:40 -0800

    Move slot regex pattern guidance into collapsible "Format" section

--- src/components/SchemaSlotHelp/SchemaSlotHelp.tsx
+++ src/components/SchemaSlotHelp/SchemaSlotHelp.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { SlotDefinition } from "../../linkml-metamodel";
+import { IonAccordion, IonAccordionGroup, IonItem, IonLabel } from "@ionic/react";
+import Pluralize from "../Pluralize/Pluralize";
 
 interface SchemaSlotHelpProps {
   slot: SlotDefinition;
@@ -9,15 +11,16 @@ const SchemaSlotHelp: React.FC<SchemaSlotHelpProps> = ({
   slot,
   containingObjectName = "record",
 }) => {
+  // Get any pattern information present in this slot definition.
+  const pattern = slot.pattern;
+  const hasPattern = typeof pattern === "string";
+  const structuredPatternSyntax = slot.structured_pattern?.syntax;
+  const hasStructuredPatternSyntax = typeof structuredPatternSyntax === "string";
+
+  // Process other types of guidance present on this slot's specification.
   const guidanceParagraphs: string[] = [];
   if (slot.comments && slot.comments.length) {
     guidanceParagraphs.concat(...slot.comments);
-  }
-  if (slot.pattern) {
-    guidanceParagraphs.push("Pattern as regular expression: " + slot.pattern);
-  }
-  if (slot.structured_pattern) {
-    guidanceParagraphs.push("Pattern hint: " + slot.structured_pattern.syntax);
   }
   const hasMinValue = slot.minimum_value != null;
   const hasMaxValue = slot.maximum_value != null;
@@ -72,6 +75,36 @@ const SchemaSlotHelp: React.FC<SchemaSlotHelpProps> = ({
           </ul>
         </>
       )}
+
+      {/* If we have any patterns information, display it in this section. */}
+      {hasPattern || hasStructuredPatternSyntax ? (
+        <>
+          <h4>Format</h4>
+          <IonAccordionGroup>
+            <IonAccordion>
+              <IonItem slot={"header"}>
+                <IonLabel>Requirements</IonLabel>
+              </IonItem>
+              <div className={"ion-padding"} slot={"content"}>
+                <p>
+                  {/* TODO: Check what this looks like when both types of patterns are present (is that allowed?). */}
+                  <Pluralize
+                    count={hasPattern && hasStructuredPatternSyntax ? 2 : 1}
+                    singular={
+                      "The value must conform to this regular expression pattern:"
+                    }
+                    plural={
+                      "The value must conform to these regular expression patterns:"
+                    }
+                  />
+                </p>
+                {hasPattern && <p>{pattern}</p>}
+                {hasStructuredPatternSyntax && <p>{structuredPatternSyntax}</p>}
+              </div>
+            </IonAccordion>
+          </IonAccordionGroup>
+        </>
+      ) : null}
     </>
   );
 };
