7c7cd4052f9fc5394afa76ab2d621b01fa6b9dc3
eecavanna <eecavanna@users.noreply.github.com>
2025-02-02 15:56:25 -0800

    PoC: Use `regex-colorizer` (version `v0.3.1`) to colorize regex pattern

--- src/components/SchemaSlotHelp/RegexPattern.tsx
+++ src/components/SchemaSlotHelp/RegexPattern.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import styles from "./RegexPattern.module.css";
 
+// @ts-expect-error Version v0.3.1 of `regex-colorizer` is from 2012 and does not include type information.
+import { colorizeText } from "regex-colorizer";
+
 interface RegexPatternProps {
   pattern: string;
 }
 
 const RegexPattern: React.FC<RegexPatternProps> = ({ pattern }) => {
-  return <div className={styles.pattern}>{pattern}</div>;
+  // Note: The `colorizeText` function will wrap various parts of the pattern within HTML elements, some of which
+  //       have classes defined on them. We can then style those HTML elements (see `RegexPattern.module.css`).
+  const htmlWeTrust = { __html: colorizeText(pattern) };
+  return <div className={styles.pattern} dangerouslySetInnerHTML={htmlWeTrust}></div>;
 };
 
 export default RegexPattern;
