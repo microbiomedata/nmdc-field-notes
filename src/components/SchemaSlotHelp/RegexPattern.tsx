bfc8b7ce62768838ecc69e963f6294f0fb3806f9
eecavanna <eecavanna@users.noreply.github.com>
2025-02-03 14:36:50 -0800

    Uninstall and un-integrate the outdated `regex-colorizer` package

--- src/components/SchemaSlotHelp/RegexPattern.tsx
+++ src/components/SchemaSlotHelp/RegexPattern.tsx
@@ -1,18 +1,12 @@
 import React from "react";
 import styles from "./RegexPattern.module.css";
 
-// @ts-expect-error Version v0.3.1 of `regex-colorizer` is from 2012 and does not include type information.
-import { colorizeText } from "regex-colorizer";
-
 interface RegexPatternProps {
   pattern: string;
 }
 
 const RegexPattern: React.FC<RegexPatternProps> = ({ pattern }) => {
-  // Note: The `colorizeText` function will wrap various parts of the pattern within HTML elements, some of which
-  //       have classes defined on them. We can then style those HTML elements (see `RegexPattern.module.css`).
-  const htmlWeTrust = { __html: colorizeText(pattern) };
-  return <div className={styles.pattern} dangerouslySetInnerHTML={htmlWeTrust}></div>;
+  return <div className={`${styles.pattern} ion-padding`}>{pattern}</div>;
 };
 
 export default RegexPattern;
