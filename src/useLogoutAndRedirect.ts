dd7ef173c7e249fb192fa6f80fd6ee9a3dc04a80
Patrick Kalita <pkalita@lbl.gov>
2025-02-21 10:30:53 -0800

    Do not present multiple session expired toasts

--- src/useLogoutAndRedirect.ts
+++ src/useLogoutAndRedirect.ts
@@ -8,9 +8,12 @@ import { close } from "ionicons/icons";
 function useLogoutAndRedirect() {
   const history = useHistory();
   const [present] = useIonToast();
-  const { logout } = useStore();
+  const { logout, isLoggedIn } = useStore();
 
   const logoutAndRedirect = useCallback(async () => {
+    if (!isLoggedIn) {
+      return;
+    }
     await logout();
     void present({
       message: "Your session has expired. Please log in again.",
@@ -24,7 +27,7 @@ function useLogoutAndRedirect() {
       ],
     });
     history.replace(paths.welcome);
-  }, [history, logout, present]);
+  }, [history, isLoggedIn, logout, present]);
 
   return { logoutAndRedirect };
 }
