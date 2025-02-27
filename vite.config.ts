20e9926ea330e3be338a9b540acd545cac85f791
Patrick Kalita <pkalita@lbl.gov>
2025-02-27 15:18:57 -0800

    Remove unnecessary assetsInclude config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), legacy()],
-  assetsInclude: ["**/*.md"],
   test: {
     globals: true,
     environment: "jsdom",
