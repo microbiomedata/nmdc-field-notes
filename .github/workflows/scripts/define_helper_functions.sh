0f94f60278a7594cd0c15a15613961d5ca35da14
eecavanna <eecavanna@users.noreply.github.com>
2025-07-21 20:23:54 -0700

    Extract additional inline script into helper function

--- .github/workflows/scripts/define_helper_functions.sh
+++ .github/workflows/scripts/define_helper_functions.sh
@@ -5,7 +5,7 @@
 # Usage: decode_and_decrypt_keystore_file <file_content_encrypted_encoded> <decryption_key> <temp_dir_path> <output_file_path>
 ###############################################################################
 decode_and_decrypt_file() {
-    # Extract parameters into local variables and print their lengths as a sanity check.
+    # Extract parameters into local variables and print their lengths (for sensitive values) or values.
     local file_content_encrypted_encoded="${1}"
     local decryption_key="${2}"
     local temp_dir_path="${3}"
@@ -29,3 +29,30 @@ decode_and_decrypt_file() {
     mv "${temp_dir_path}/file" "${output_file_path}"
     stat "${output_file_path}"
 }
+
+###############################################################################
+# Creates a `keystore.properties` file with the provided keystore details.
+# Usage: create_keystore_properties_file <keystore_password> <key_password> <key_alias> <keystore_file_path> <keystore_properties_file_path>
+###############################################################################
+create_keystore_properties_file() {
+    # Extract parameters into local variables and print their lengths (for sensitive values) or values.
+    local keystore_password="${1}"
+    local key_password="${2}"
+    local key_alias="${3}"
+    local keystore_file_path="${4}"
+    local keystore_properties_file_path="${5}"
+    echo "Keystore password (number of characters) : ${#keystore_password}"
+    echo "Key password (number of characters)      : ${#key_password}"
+    echo "Key alias                                : ${key_alias}"
+    echo "Keystore file path                       : ${keystore_file_path}"
+    echo "Keystore properties file path            : ${keystore_properties_file_path}"
+
+    # Create the `keystore.properties` file with the provided values.
+    echo "Creating keystore.properties file."
+    echo "storePassword=${keystore_password}" > "${keystore_properties_file_path}"
+    echo "keyPassword=${key_password}"       >> "${keystore_properties_file_path}"
+    echo "keyAlias=${key_alias}"             >> "${keystore_properties_file_path}"
+    echo "storeFile=${keystore_file_path}"   >> "${keystore_properties_file_path}"
+    wc -l "${keystore_properties_file_path}"
+}
+
