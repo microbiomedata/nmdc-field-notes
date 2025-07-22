9027197d005fed9863a3036b97bf279b92bd0d9f
eecavanna <eecavanna@users.noreply.github.com>
2025-07-21 22:51:39 -0700

    Extract another inline script into a bash function

--- .github/workflows/scripts/define_helper_functions.sh
+++ .github/workflows/scripts/define_helper_functions.sh
@@ -53,3 +53,38 @@ create_keystore_properties_file() {
     wc -l "${keystore_properties_file_path}"
 }
 
+###############################################################################
+# Adds lines that reference the `keystore.properties` file, to the top of the
+# `build.gradle` file.
+# Note: To effectively "prepend" the lines, we first write everything to a
+#       temporary file in the order we want, then we overwrite the target file
+#       with the contents of that temporary file.
+# Usage: inject_keystore_properties_reference_into_build_gradle_file <keystore_properties_file_path> <temp_dir_path> <build_gradle_file_path>
+###############################################################################
+inject_keystore_properties_reference_into_build_gradle_file() {
+    # Extract parameters into local variables and print their values.
+    local keystore_properties_file_path="${1}"
+    local build_gradle_file_path="${2}"
+    local temp_dir_path="${3}"
+    echo "Keystore properties file path : ${keystore_properties_file_path}"
+    echo "build.gradle file path        : ${build_gradle_file_path}"
+    echo "Temporary directory path      : ${temp_dir_path}"
+
+    # Add lines that reference the `keystore.properties` file, to the top of the `build.gradle` file.
+    echo "Injecting keystore properties reference into build.gradle file."
+
+    # Create a temporary file with the additional lines.
+    echo '
+        def keystorePropertiesFile = rootProject.file("keystore.properties")
+        def keystoreProperties = new Properties()
+        keystoreProperties.load(new FileInputStream(keystorePropertiesFile))
+    ' > "${temp_dir_path}/build.gradle"
+    
+    # Append the original `build.gradle` content to the temporary file.
+    cat "${build_gradle_file_path}" >> "${temp_dir_path}/build.gradle"
+
+    # Move the temporary file back to the original `build.gradle` file location.
+    echo "Length of original build.gradle file: $(wc -l < "${build_gradle_file_path}")"
+    mv "${temp_dir_path}/build.gradle" "${build_gradle_file_path}"
+    echo "Length of updated build.gradle file: $(wc -l < "${build_gradle_file_path}")"
+}
