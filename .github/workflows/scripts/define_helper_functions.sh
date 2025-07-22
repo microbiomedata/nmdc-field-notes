7ce25a3f8c00bb4b3b2912784d15dfe6648ee6b1
eecavanna <eecavanna@users.noreply.github.com>
2025-07-21 20:30:36 -0700

    Extract file contents to temporary directory, not cwd

--- .github/workflows/scripts/define_helper_functions.sh
+++ .github/workflows/scripts/define_helper_functions.sh
@@ -23,7 +23,7 @@ decode_and_decrypt_file() {
     stat "${temp_dir_path}/file.tar.gz.enc"
 
     echo "Decrypting file content."
-    echo "${decryption_key}" | openssl enc -d -aes256 -pbkdf2 -in "${temp_dir_path}/file.tar.gz.enc" | tar xz
+    echo "${decryption_key}" | openssl enc -d -aes256 -pbkdf2 -in "${temp_dir_path}/file.tar.gz.enc" | tar xz -C "${temp_dir_path}"
     stat "${temp_dir_path}/file"
 
     mv "${temp_dir_path}/file" "${output_file_path}"
