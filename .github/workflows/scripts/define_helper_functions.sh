a79c39cd16a08a6203acb769b8caae55238ea4a4
eecavanna <eecavanna@users.noreply.github.com>
2025-07-21 20:37:37 -0700

    Debugging: List files in related directories

--- .github/workflows/scripts/define_helper_functions.sh
+++ .github/workflows/scripts/define_helper_functions.sh
@@ -24,6 +24,10 @@ decode_and_decrypt_file() {
 
     echo "Decrypting file content."
     echo "${decryption_key}" | openssl enc -d -aes256 -pbkdf2 -in "${temp_dir_path}/file.tar.gz.enc" | tar xz -C "${temp_dir_path}"
+    echo "temp dir files"
+    ls -l "${temp_dir_path}"
+    echo "cwd files"
+    ls -l
     stat "${temp_dir_path}/file"
 
     mv "${temp_dir_path}/file" "${output_file_path}"
